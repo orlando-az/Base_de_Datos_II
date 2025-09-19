b473fb954ba211e90125b6dd91e59ef86d0306b5
Isaac Aguilera <Isaac.Aguilera@upds.edu.bo>
2025-09-19 18:24:32 -0400

    Refactor exercise titles for clarity and consistency in SQL script

--- exercises/03_subquery.sql
+++ exercises/03_subquery.sql
@@ -6,12 +6,25 @@
 -- Para cada salesperson (vendedor) calcular su suma de totaldue y mostrar sólo los vendedores 
 -- cuya suma es mayor que el promedio de sumas entre todos los vendedores.
 
--- Ejemplo 3 — Diferencia entre el pedido y el promedio del cliente
+-- Ejercicio 3 — Diferencia entre el pedido y el promedio del cliente
 -- Mostrar salesorderid, customerid, totaldue y la diferencia respecto al promedio de pedidos de ese cliente.
 
--- Ejemplo 4 — Producto más caro por cliente
+-- Ejercicio 4 — Producto más caro por cliente
 -- Muestre, para cada cliente, el nombre del producto de mayor precio unitario que haya adquirido.
 
 -- Ejemplo 5 — Empleado con mayor antigüedad en cada departamento por turno (2014)
 -- Para cada departamento y turno, mostrar el empleado con mayor antigüedad, indicando su nombre completo y la fecha de inicio en el departamento.
 -- Usa subconsulta correlacionada para obtener la fecha mínima de inicio.
+
+-- Ejemplo 6 
+
+-- Muestra el listado de clientes el total gastado en las gestion 2012, 2013 y 2014
+
+-- Salida:
+-- nombre          2012    2013   2014
+-- Juan Perez | 25697.23 |  0.0 | 47852.58
+
+
+-- Ejercicio 5 — Empleados que nunca cambiaron de departamento
+
+-- Devolver empleados que solo tengan una única asignación de departamento
