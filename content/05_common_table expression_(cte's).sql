34731a0ce224444e7be8a1a51a98a2624a51c4f8
orlando-az <tj.orlando.aguilera.z@upds.net.bo>
2025-09-22 19:50:45 -0400

    ejemplo

--- content/05_common_table expression_(cte's).sql
+++ content/05_common_table expression_(cte's).sql
@@ -1,8 +1,35 @@
 -- Ejercicio 1: 
--- Calcular el stock total de cada producto sumando las cantidades disponibles 
--- en ProductInventory y mostrar el ID y nombre del producto junto con su total 
--- de stock, ordenando los resultados de mayor a menor cantidad.
-
+-- Calcular el stock total de cada producto sumando 
+-- las cantidades disponibles 
+-- en ProductInventory y mostrar el ID y 
+-- nombre del producto junto con su total 
+-- de stock, ordenando los resultados de mayor a 
+-- menor cantidad.
+WITH stock as(
+SELECT pi.productid,sum(pi.quantity) as total_stock ,
+p.productsubcategoryid
+from 
+production.productinventory pi
+INNER JOIN production.product p on p.productid= pi.productid
+group by pi.productid,p.productsubcategoryid
+),
+max_total as (
+ select psc.name as sub_categoria,
+ pc.name as categoria,
+ max(s.total_stock) as total_max,
+ psc.productsubcategoryid as subcategoryid
+ from production.productsubcategory psc
+ INNER JOIN production.productcategory pc on psc.productcategoryid =
+ pc.productcategoryid 
+ INNER JOIN stock s on s.productsubcategoryid = psc.productcategoryid
+ group by psc.name,pc."name",subcategoryid
+)
+SELECT p.name as producto, s.total_stock, mt.total_max
+subcategoria, categoria
+FROM production.product p
+INNER JOIN stock s on p.productid= s.productid
+INNER JOIN max_total mt on mt.subcategoryid = 
+p.productsubcategoryid
 -- Ejercicio 2:
 -- Calcular la antigüedad de los empleados en años, meses y días, 
 -- usando startdate y enddate (o la fecha actual si no hay fin), 
