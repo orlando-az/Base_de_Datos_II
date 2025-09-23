1950ec83be8ee5fbfe45f03bf2118d5c2904f5d1
Isaac Aguilera <Isaac.Aguilera@upds.edu.bo>
2025-09-23 17:48:20 -0400

    exercises function

---
+++ exercises/05_function_user.sql
@@ -0,0 +1,8 @@
+-- Ejercicio 1
+-- Genera una función para calcular el promedio de ventas de un cliente en un año
+
+-- Ejercicio 2
+-- Crea una función para calcular la edad de los clientes
+
+-- Ejercicio 3
+-- Define una función para obtener el total de productos vendidos de un producto en especificio.
