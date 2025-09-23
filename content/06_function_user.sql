cda3107c3166ffc4785c89eeec2fd44fd5b84f13
Isaac Aguilera <Isaac.Aguilera@upds.edu.bo>
2025-09-23 17:12:39 -0400

    function and procedure

---
+++ content/06_function_user.sql
@@ -0,0 +1,37 @@
+-- Ejercicio 1:
+-- Cree una función llamada fn_fullname
+-- que reciba como parámetro el identificador de una persona y retorne el nombre completo de esa persona.
+-- CREATE OR REPLACE FUNCTION ()
+-- RETURNS TEXT
+-- LANGUAGE plpgsql
+-- AS $$
+-- DECLARE
+-- BEGIN
+--   ;
+--   RETURN;
+-- END;
+-- $$;
+
+
+-- Ejercicio 2
+-- Crear una función total_customer que reciba como parámetros el id de cliente y un año, 
+-- y retorne el total de compras realizadas por ese cliente en ese año.
+
+
+-- Ejercicio 3
+-- Definir una función  que tome como entrada el id de un producto y devuelva una tabla con el id
+-- y el nombre de los proveedores asociados.
+-- CREATE OR REPLACE FUNCTION ()
+-- RETURNS TABLE()
+-- LANGUAGE plpgsql
+-- AS $$
+-- BEGIN
+--   RETURN QUERY
+-- END;
+-- $$;
+
+-- Ejercicio 4
+-- Crear una función que reciba un id del cliente y un año, y devuelva la categoría del cliente 
+-- (Premium, Regular o Básico) según el total de compras en ese año.
+
+
