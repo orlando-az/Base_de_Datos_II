3bb3c9db4aa46b08ab30f7b7e285c1464070bd14
orlando-az <tj.orlando.aguilera.z@upds.net.bo>
2025-09-22 20:21:50 -0400

    optimo

--- content/05_common_table expression_(cte's).sql
+++ content/05_common_table expression_(cte's).sql
@@ -30,6 +30,58 @@ FROM production.product p
 INNER JOIN stock s on p.productid= s.productid
 INNER JOIN max_total mt on mt.subcategoryid = 
 p.productsubcategoryid
+
+
+WITH stock as(
+SELECT pi.productid,sum(pi.quantity) as total_stock 
+from 
+production.productinventory pi
+INNER JOIN production.product p on p.productid= pi.productid
+),
+max_total as (
+ select psc.name as sub_categoria,
+ pc.name as categoria,
+ max(s.total_stock) as total_max,
+ psc.productsubcategoryid as subcategoryid
+ from production.productsubcategory psc
+ INNER JOIN production.productcategory pc on psc.productcategoryid =
+ pc.productcategoryid 
+ INNER JOIN stock s on s.productsubcategoryid = psc.productcategoryid
+ group by psc.name,pc."name",subcategoryid
+)
+SELECT p.name as producto, s.total_stock, mt.total_max
+subcategoria, categoria
+FROM production.product p
+INNER JOIN stock s on p.productid= s.productid
+INNER JOIN max_total mt on mt.subcategoryid = 
+p.productsubcategoryid
+
+
+segunda forma
+
+WITH stock as(
+SELECT pi.productid,sum(pi.quantity) as total_stock 
+from  production.productinventory pi
+INNER JOIN production.product p on p.productid= pi.productid
+GROUP by pi.productid
+),
+max_total as (
+	select p.productsubcategoryid as max_subcateria_id, max(s.total_stock) as max_total 
+	FROM stock s
+	INNER JOIN production.product p on p.productid = s.productid
+	group by p.productsubcategoryid
+)
+SELECT p.name as producto,
+psc.name as subcategoria,pc.name as categoria,
+s.total_stock, mt.max_total
+FROM production.product p
+INNER JOIN production.productsubcategory psc on 
+psc.productsubcategoryid = p.productsubcategoryid
+INNER JOIN production.productcategory pc on 
+pc.productcategoryid= psc.productcategoryid
+INNER JOIN stock s on s.productid = p.productid
+INNER JOIN max_total mt on mt.max_subcateria_id = p.productsubcategoryid
+
 -- Ejercicio 2:
 -- Calcular la antigüedad de los empleados en años, meses y días, 
 -- usando startdate y enddate (o la fecha actual si no hay fin), 
