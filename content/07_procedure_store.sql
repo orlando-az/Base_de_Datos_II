fa8c8badd4268b1492b7930c262996d6ee940157
orlando-az <tj.orlando.aguilera.z@upds.net.bo>
2025-09-25 08:57:25 -0400

    procedure exercise

--- content/07_procedure_store.sql
+++ content/07_procedure_store.sql
@@ -5,9 +5,64 @@
 -- Inserte un nuevo estudiante en la tabla student.
 -- Muestre el ID generado y el nombre completo.
 
+    CREATE or REPLACE PROCEDURE ()
+    LANGUAGE plpgsql
+    as $$
+    DECLARE
+    ;
+    BEGIN
+    END;
+    $$
+
+CREATE or REPLACE PROCEDURE add_student(
+	v_firstname text,
+	v_lastname text,
+	v_email text,
+	v_city text
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	v_student_id int;
+BEGIN
+	INSERT INTO student
+	(first_name,last_name,email,city,enrollment_date,status)
+	VALUES(v_firstname,v_lastname,v_email,v_city,current_date,'active')
+	RETURNING id INTO v_student_id;
+
+	RAISE NOTICE 'ID student: %',v_student_id;
+END;
+$$
+CALL add_student('juan','torrez','juan.torrez1@gmail.com','Tarija')
+
+SELECT * from
+student
+
+
 -- Ejercicio 2
--- Crea un procedimiento almacenado que reciba el ID de un estudiante y un nuevo estado.
--- El procedimiento debe actualizar el campo status en la tabla student y mostrar un mensaje confirmando el cambio.
+-- Crea un procedimiento almacenado que reciba el ID de un estudiante 
+-- y un nuevo estado.
+-- El procedimiento debe actualizar el campo status en 
+-- la tabla student y mostrar un mensaje confirmando el cambio.
+
+CREATE OR REPLACE PROCEDURE update_status(
+	v_student_id int,
+	v_status text
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	update student
+	set status=v_status
+	where id = v_student_id;
+
+	RAISE NOTICE 'El estudiante: % fue actualizado con el estado: %',
+	v_student_id,v_status;
+END;
+$$
+CALL update_status(3,'active')
+
+SELECT * from student
 
 -- Ejercicio 3
 -- Crea un procedimiento almacenado que:
