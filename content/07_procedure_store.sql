b2e308ecc6892fccd92119571e2f579e869f5067
orlando-az <tj.orlando.aguilera.z@upds.net.bo>
2025-09-26 17:54:58 -0400

    commit procedure

--- content/07_procedure_store.sql
+++ content/07_procedure_store.sql
@@ -66,20 +66,67 @@ SELECT * from student
 
 -- Ejercicio 3
 -- Crea un procedimiento almacenado que:
-
--- Reciba los datos de un estudiante (nombre, apellido, email y ciudad) y el ID de una sección por defecto.
+-- Reciba los datos de un estudiante (nombre, apellido, email y ciudad) 
+-- y el ID de una sección por defecto.
 -- Inserte el nuevo estudiante en la tabla student con la fecha actual y estado 'active'.
--- Matricule automáticamente al estudiante en la sección indicada insertando en la tabla enrollment.
+-- Matricule automáticamente al estudiante en la sección indicada insertando 
+-- en la tabla enrollment.
 -- Muestre un mensaje confirmando el ID del estudiante y el ID de la matrícula generados.
 
+CREATE or REPLACE PROCEDURE student_enrollment(
+	v_firstname text, v_lastname text,v_email text,
+	v_city text,
+	v_section_id int
+)
+LANGUAGE plpgsql
+as $$
+DECLARE
+	v_student_id int;
+BEGIN
+	insert into student(first_name,last_name,email,city,enrollment_date,status)
+	VALUES(v_firstname,v_lastname,v_email,v_city,current_date,'active')
+	RETURNING id INTO v_student_id;
 
+	INSERT INTO enrollment(student_id,section_id,enrolled_on,status)
+	VALUES(v_student_id,v_section_id,current_date,'enrolled');
+	RAISE NOTICE 'El estudiante % % esta registrado en seccion: %',
+	v_firstname,v_lastname,v_section_id;
+END;
+$$
+CALL student_enrollment('julian','flores','julian.flores@gmail.com','Beni',1)
 
 -- Ejercicio 4
 -- Crea un procedimiento almacenado llamado enroll_if_not_exists que:
 
 -- Reciba el ID de un estudiante y el ID de una sección.
 -- Verifique si el estudiante ya está matriculado en esa sección.
--- Si no lo está, lo inserte en la tabla enrollment con fecha actual y estado 'enrolled'.
--- Si ya existe la matrícula, muestre un mensaje indicando que el estudiante ya está matriculado.
+-- Si no lo está, lo inserte en la tabla enrollment con fecha actual 
+-- y estado 'enrolled'.
+-- Si ya existe la matrícula, muestre un mensaje indicando que el 
+-- estudiante ya está matriculado.
 -- Mostrar el resultado de la operación.
 
+CREATE OR REPLACE PROCEDURE enroll_if_not_exists(
+	v_student_id int, v_section_id int
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	v_count int;
+BEGIN
+SELECT count(*) INTO v_count
+FROM enrollment
+where student_id = v_student_id and section_id = v_section_id;
+
+if v_count = 0 then
+	insert into enrollment(student_id,section_id,enrolled_on,status)
+	VALUES(v_student_id,v_section_id,CURRENT_DATE, 'enrolled');
+	
+	RAISE NOTICE 'El estudiante fue registrado';
+else 
+	RAISE NOTICE 'El estudiante ya se encuentra registrado';
+END IF;
+END;
+$$
+
+CALL enroll_if_not_exists(2,1)
