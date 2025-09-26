6b6301f164a844d5c52cef782e0c3dee23d9a6c1
Isaac Aguilera <Isaac.Aguilera@upds.edu.bo>
2025-09-26 18:40:49 -0400

    Triggers

--- content/09_triggers.sql
+++ content/09_triggers.sql
@@ -3,6 +3,20 @@
 -- verifique si grade es menor que 0 y, en ese caso, lo ajuste automáticamente a 0.
 -- Luego, crear un trigger que ejecute esta función en cada fila.
 
+-- CREATE OR REPLACE FUNCTION ()
+-- RETURNS TRIGGER 
+-- LANGUAGE plpgsql
+-- AS $$
+-- BEGIN
+-- END;
+-- $$
+
+-- CREATE TRIGGER 
+-- BEFORE INSERT OR UPDATE ON 
+-- FOR EACH ROW
+-- EXECUTE FUNCTION
+
+
 -- Ejercicio 2
 -- Crear un trigger que muestre un aviso cada vez que se inserte una 
 -- sección de clase (class_section) sin un instructor asignado (instructor_id es NULL).
